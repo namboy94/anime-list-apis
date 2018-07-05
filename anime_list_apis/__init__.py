b1a06903c091791d9277a112e3a6df7f7a411c5f
Hermann Krumrey <hermann@krumreyh.com>
2018-07-05 10:27:17 +0200

    Set up project

---
+++ anime_list_apis/__init__.py
@@ -0,0 +1,18 @@
+"""LICENSE
+Copyright 2018 Hermann Krumrey <hermann@krumreyh.com>
+
+This file is part of anime-list-apis.
+
+anime-list-apis is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+anime-list-apis is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with anime-list-apis.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE"""
---
+++ doc/sphinx/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to toktokkie's documentation!
+=====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   toktokkie.rst
+   modules.rst
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
